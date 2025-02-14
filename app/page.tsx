6796a3b24d248c78e7ac699a19297fa325a6a395
Eli <78963198+niceEli@users.noreply.github.com>
2025-02-14 18:10:38 -0500

    feat: mobile support

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 
 export default function Page() {
   return <>
-    <div className={"flex w-screen h-screen items-center justify-center"}>
+    <div className={"flex w-screen h-screen items-center justify-center p-4"}>
       <div className={"text-center"}>
-        <div className={"rounded-2xl bg-emerald-700 rotate-6"}>
-          <h1 className={"text-9xl text-yellow-100 font-extrabold font-serif py-5 m-5 -rotate-6"}>DogThing</h1>
+        <div className={"rounded-2xl bg-emerald-700 rotate-6 !sm:max-w-xs mx-auto"}>
+          <h1 className={"text-6xl sm:text-9xl text-yellow-100 font-extrabold font-serif py-5 m-5 -rotate-6"}>DogThing</h1>
         </div>
         <a className={"btn btn-warning"} href={"/dawg"}>Get A Dog</a>
       </div>
